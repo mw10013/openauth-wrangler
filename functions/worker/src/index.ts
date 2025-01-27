fb528f2c1831964265b957fa3db778984d665cea
Michael Wu <mw10013@gmail.com>
2025-01-26 19:00:47 -0500

    remove fe

--- functions/worker/src/index.ts
+++ functions/worker/src/index.ts
@@ -1,10 +1,8 @@
 import { issuer } from '@openauthjs/openauth'
-import { Client, createClient } from '@openauthjs/openauth/client'
 import { PasswordProvider } from '@openauthjs/openauth/provider/password'
 import { CloudflareStorage } from '@openauthjs/openauth/storage/cloudflare'
 import { PasswordUI } from '@openauthjs/openauth/ui/password'
 import { subjects } from '@repo/shared/subjects'
-import { Hono } from 'hono'
 
 async function getUser(email: string) {
 	// Get user from database
@@ -14,7 +12,7 @@ async function getUser(email: string) {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-		const openauth = issuer({
+		return issuer({
 			storage: CloudflareStorage({
 				namespace: env.KV,
 			}),
@@ -36,69 +34,6 @@ export default {
 				}
 				throw new Error('Invalid provider')
 			},
-		})
-
-		// Impossible to extend with additional routes:https://github.com/openauthjs/openauth/issues/127#issuecomment-2569976202
-		// https://hono.dev/docs/api/routing#grouping
-		const fe = new Hono<{
-			Variables: {
-				client: Client
-				redirectUri: string
-			}
-		}>().basePath('/fe')
-		fe.use(async (c, next) => {
-			const { origin } = new URL(c.req.url)
-			const client = createClient({
-				clientID: 'fe',
-				fetch: async (input, init) => {
-					const request = new Request(input, init)
-					return openauth.fetch(request, env, ctx)
-				},
-				issuer: origin,
-			})
-			c.set('client', client)
-			c.set('redirectUri', origin + '/fe/callback')
-			await next()
-		})
-		fe.get('/callback', async (c) => {
-			try {
-				const url = new URL(c.req.url)
-				const code = url.searchParams.get('code')!
-				const exchanged = await c.var.client.exchange(code, c.var.redirectUri)
-				if (exchanged.err) throw new Error('Invalid code')
-				const response = new Response(null, { status: 302, headers: {} })
-				response.headers.set('Location', url.origin + '/fe')
-				setSession(response, exchanged.tokens.access, exchanged.tokens.refresh)
-				return response
-			} catch (e: any) {
-				return new Response(e.toString())
-			}
-		})
-		fe.get('/authorize', async (c) => Response.redirect(await c.var.client.authorize(c.var.redirectUri, 'code').then((v) => v.url), 302))
-		fe.get('/', async (c) => {
-			const cookies = new URLSearchParams(request.headers.get('cookie')?.replaceAll('; ', '&'))
-			const verified = await c.var.client.verify(subjects, cookies.get('access_token')!, {
-				refresh: cookies.get('refresh_token') || undefined,
-			})
-			if (verified.err) return Response.redirect(new URL(c.req.url).origin + '/fe/authorize', 302)
-			const response = Response.json(verified.subject)
-			if (verified.tokens) setSession(response, verified.tokens.access, verified.tokens.refresh)
-			return response
-		})
-
-		const app = new Hono()
-		app.route('/', fe)
-		app.route('/', openauth) // Mount last
-		return app.fetch(request, env, ctx)
+		}).fetch(request, env, ctx)
 	},
 } satisfies ExportedHandler<Env>
-
-// https://github.com/openauthjs/openauth/blob/master/examples/client/cloudflare-api/api.ts
-function setSession(response: Response, access: string, refresh: string) {
-	if (access) {
-		response.headers.append('Set-Cookie', `access_token=${access}; HttpOnly; SameSite=Strict; Path=/; Max-Age=2147483647`)
-	}
-	if (refresh) {
-		response.headers.append('Set-Cookie', `refresh_token=${refresh}; HttpOnly; SameSite=Strict; Path=/; Max-Age=2147483647`)
-	}
-}
