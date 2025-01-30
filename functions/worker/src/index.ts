12c726087f3e086d5a893c7661457ed751a2d762
Michael Wu <mw10013@gmail.com>
2025-01-29 19:28:36 -0500

    code

--- functions/worker/src/index.ts
+++ functions/worker/src/index.ts
@@ -1,6 +1,8 @@
 import { issuer } from '@openauthjs/openauth'
+import { CodeProvider } from '@openauthjs/openauth/provider/code'
 import { PasswordProvider } from '@openauthjs/openauth/provider/password'
 import { CloudflareStorage } from '@openauthjs/openauth/storage/cloudflare'
+import { CodeUI } from '@openauthjs/openauth/ui/code'
 import { PasswordUI } from '@openauthjs/openauth/ui/password'
 import { subjects } from '@repo/shared/subjects'
 
@@ -16,15 +18,31 @@ export default {
 			}),
 			subjects,
 			providers: {
+				code: CodeProvider(
+					CodeUI({
+						copy: {
+							code_info: "We'll send a pin code to your email"
+						},
+						sendCode: async (claims, code) => console.log(claims.email, code)
+					})
+				),
 				password: PasswordProvider(
 					PasswordUI({
 						sendCode: async (email, code) => {
 							console.log(email, code)
 						},
+						copy: {
+							input_code: 'Code (check Worker logs)',
+						},
 					}),
 				),
 			},
 			success: async (ctx, value) => {
+				if (value.provider === 'code') {
+					return ctx.subject('user', {
+						userId: value.claims.email,
+					})
+				}
 				if (value.provider === 'password') {
 					return ctx.subject('user', {
 						userId: value.email,
